eeb6641e4f958678aae9497df39329b62aa2a5a0
Li Shi <shili2017@sjtu.edu.cn>
2020-05-11 10:19:27 +0800

    Template for frontend/backend (refer to https://scotch.io/tutorials/build-a-to-do-application-using-django-and-react) Maybe readme should be completed later.

---
+++ backend/start/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Todo  # add this
+
+
+# Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description', 'completed')  # add this
+
+
+# Register your models here.
+admin.site.register(Todo, TodoAdmin)  # add this
