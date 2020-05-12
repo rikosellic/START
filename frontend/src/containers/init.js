40367d8d500f4433e3b4315fa51fea69fb561c2d
Li Shi <shili2017@sjtu.edu.cn>
2020-05-12 22:14:47 +0800

    建了4个页面：首页面（登陆+注册按钮的那个）/注册页面/登录页面/主页面，前端组可以开始干活了！！！

---
+++ frontend/src/containers/init.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Init extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Init!</h1>
+      </div>
+    );
+  }
+}
+
+export default Init;
---
+++ frontend/src/containers/register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Register extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>This is Register!</div>
+      </div>
+    );
+  }
+}
+
+export default Register;
