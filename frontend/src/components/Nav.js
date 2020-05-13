b6b70bfb6bb43653be39feeafb0ae5f5d0976545
Li Shi <shili2017@sjtu.edu.cn>
2020-05-13 16:43:18 +0800

    Update main page. For the frontend group, please push in this branch :) Maybe later we can merge to the master branch after everything works fine

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+  Navbar,
+  Nav,
+  Container
+} from "react-bootstrap";
+
+
+const NavBar = (props) => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/main">START!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/user">个人信息</Nav.Link>
+              <Nav.Link href="/user">退出登录</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default NavBar;
--- frontend/src/containers/main.js
+++ frontend/src/containers/main.js
@@ -1,10 +1,57 @@
 import React from 'react';
+import NavBar from "../components/Nav";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+} from 'react-bootstrap';
+import "./main.css"
+import Logo from "../components/Logo";
+import Footer from "../components/Footer";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <div>This is Main!</div>
+        <NavBar/>
+        <main role="main">
+          <Jumbotron>
+            <Container>
+              <Logo/>
+              <h4>
+                这是你加入START!的第3天，你已经学习了150个单词。
+              </h4>
+              <p className="search-box">
+                <Row>
+                  <Col></Col>
+                  <Form inline>
+                    <Form.Control type="text" placeholder="🔍搜索单词" className="mr-sm-2" required/>
+                    <Button variant="success" type="submit">搜索</Button>
+                  </Form>
+                  <Col></Col>
+                </Row>
+              </p>
+              <p>
+                <Row>
+                  <Col>
+                  </Col>
+                  <div class="main-button">
+                    <Button variant="primary" size="lg">创建学习房间</Button>
+                  </div>
+                  <div class="main-button">
+                    <Button variant="primary" size="lg" class="main-button">创建学习房间</Button>
+                  </div>
+                  <Col>
+                  </Col>
+                </Row>
+              </p>
+            </Container>
+          </Jumbotron>
+        </main>
+        <Footer/>
       </div>
     );
   }
