e20a8e786c0553d2d0343594af24cb24f632b489
handsomezsk <2294146957@qq.com>
2020-05-17 20:33:31 +0800

    完善了登录注册界面，注册的post

--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -19,11 +19,11 @@ class Login extends React.Component {
           <Form>
             <Form.Group>
               <Form.Label>username</Form.Label>
-              <Form.Control placeholder="Username" />
+              <Form.Control placeholder="Username" required/>
             </Form.Group>
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" required/>
             </Form.Group>
             <Form.Group>
               <Form.Check type="checkbox" label="rememberme" />
@@ -32,7 +32,7 @@ class Login extends React.Component {
             <Button variant="primary" size="sm" type="submit">
               login
             </Button>
-            <a href="//www.baidu.com" style={{marginLeft: '5%'}}>forget password?</a>
+            <a href="//www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=%E6%80%8E%E4%B9%88%E6%B2%BB%E7%96%97%E8%84%91%E6%AE%8B&fenlei=256&rsv_pq=a680fa0c0001e6b0&rsv_t=6850bfaApw8NgPkUBq5wCWxhOpFK5TRqHtzvFpTABp1%2BSph8Qb1Zn6%2F3HWI&rqlang=cn&rsv_enter=1&rsv_dl=tb&rsv_sug3=21&rsv_sug1=15&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&inputT=12308&rsv_sug4=14886" style={{marginLeft: '5%'}}>forget password?</a>
             <a href="/register" style={{marginLeft: '5%'}}>go to register</a>
             </div>
           </Form>
