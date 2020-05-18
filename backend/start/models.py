0b6e2c7a23af4117ac0d761c6346cbe33736c113
rikosellic <514677096@qq.com>
2020-05-18 23:27:18 +0800

    完成了登录功能

--- backend/start/models.py
+++ backend/start/models.py
@@ -17,26 +17,29 @@ class Todo(models.Model):
         return self.title
 '''
 class User(models.Model):
-    """
-    创建如下几个表的字段
-    """
-    # 用户ID : 该字段为主键
-    userID = models.IntegerField('userID', primary_key=True)
-    # 用户名  最大长度20 唯一 不能为空
-    username = models.CharField('username', max_length=20, unique=True,null=False)
-    # 密码  最大长度 不能为空
-    password = models.CharField('password', max_length=20,null=False)
-    # 邮箱 最大长度30 唯一 不能为空
-    email = models.CharField('email', max_length=30, unique=True, null=False)
-    # 学习目标 最大长度100
-    goal = models.CharField('goal',  max_length=100,null=True)
+	"""
+	创建如下几个表的字段
+	"""
+	# 用户ID : 该字段为主键
+	userID = models.IntegerField('userID', primary_key=True)
+	# 用户名  最大长度20 唯一 不能为空
+	username = models.CharField('username', max_length=20, unique=True,null=False)
+	# 密码  最大长度 不能为空
+	password = models.CharField('password', max_length=20,null=False)
+	# 邮箱 最大长度30 唯一 不能为空
+	email = models.CharField('email', max_length=30, unique=True, null=False)
+	# 学习目标 最大长度100
+	goal = models.CharField('goal',  max_length=100,null=True)
+	#登陆状态
+	logined= models.BooleanField('logined', null=False, default=False)
 
 
-    class Meta:
-        db_table = 'User'
 
-    def _str_(self):
-        return self.userID
+	class Meta:
+		db_table = 'User'
+
+	def _str_(self):
+		return self.userID
 
 class Word(models.Model):
 	"""
