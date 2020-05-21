ca5eaa2e231c791819db023217788b0353896b59
handsomezsk <2294146957@qq.com>
2020-05-21 22:23:19 +0800

    5月21版本

--- frontend/src/components/Nav2.js
+++ frontend/src/components/Nav2.js
@@ -13,12 +13,6 @@ const NavBar2 = (props) => {
       <Navbar bg="light" expand="lg">
         <Container>
           <Navbar.Brand href="/main">START!</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link>sdtyb</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
--- frontend/src/containers/login.js
+++ frontend/src/containers/login.js
@@ -36,55 +36,33 @@ class Login extends React.Component {
       password: event.target.value,
     });
   }
-    login(username,password) {
-        const loginValue = {"username": username,
-            "password": password}
-        const url = " http://localhost:8000/api/login";
-        try {
-            fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-type":"application/json;charset=utf-8",
-                },
-                body: JSON.stringify(loginValue),
-            }).then(res=>{
-                if(res.status === 200){
-                    alert('Successful')
-                    this.props.history.push('/main')
-                }
-                else{
-                    alert(res)
-                }
-
-            })
+  login(username,password) {
+    const loginValue = {"username": username,
+    "password": password}
+    const url = " http://localhost:8000/api/login";
+    try {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type":"application/json;charset=utf-8",
+        },
+        body: JSON.stringify(loginValue),
+        }).then(res=>{
+          if(res.status === 200){
+            alert('Successful')
+            this.props.history.push('/main')
+          }
+          else{
+            alert(res)
+          }
+        })
         } catch (error) {
-        }
+      }
     }
-    register(username,password,email) {
-        const registerValue = {"username": username,
-            "password": password,
-            "email":email}
-        const url = " http://localhost:8000/api/userinfomanagement";
-        try {
-            fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-type":"application/json;charset=utf-8",
-                },
-                body: JSON.stringify(registerValue),
-            }).then(res=>{
-                if(res.status === 200){
-                    alert('Successful')
-                    this.props.history.push('/login')
-                }
-                else{
-                    alert(res)
-                }
-
-            })
-        } catch (error) {
-        }
-    }
+  toOrderDetail(item){
+  //this.props.navigation.push('跳转的目标页面', { 参数名: 参数值 })
+  this.props.navigation.push('/personal', { username: this.username })
+  }
   render() {
     const{username,password}=this.state
     return (
@@ -104,11 +82,11 @@ class Login extends React.Component {
               <Form.Check type="checkbox" label="rememberme" />
             </Form.Group>
             <div className="login-button">
-            <Button variant="primary" size="sm" block onClick={this.login.bind(this, username, password)}>
+            <Button variant="primary" size="sm" onClick={this.login.bind(this, username, password)}>
               login
             </Button>
             <span class="blue">
-              <a href="//www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=%E6%80%8E%E4%B9%88%E6%B2%BB%E7%96%97%E8%84%91%E6%AE%8B&fenlei=256&rsv_pq=a680fa0c0001e6b0&rsv_t=6850bfaApw8NgPkUBq5wCWxhOpFK5TRqHtzvFpTABp1%2BSph8Qb1Zn6%2F3HWI&rqlang=cn&rsv_enter=1&rsv_dl=tb&rsv_sug3=21&rsv_sug1=15&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&inputT=12308&rsv_sug4=14886" style={{marginLeft: '5%'}}>forget password?</a>
+              <a href="//www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=%E6%80%8E%E4%B9%88%E6%B2%BB%E7%96%97%E8%84%91%E6%AE%8B&fenlei=256&rsv_pq=a680fa0c0001e6b0&rsv_t=6850bfaApw8NgPkUBq5wCWxhOpFK5TRqHtzvFpTABp1%2BSph8Qb1Zn6%2F3HWI&rqlang=cn&rsv_enter=1&rsv_dl=tb&rsv_sug3=21&rsv_sug1=15&rsv_sug7=100&rsv_sug2=0&rsv_btype=i&inputT=12308&rsv_sug4=14886" style={{marginLeft: '3%'}}>forget password?</a>
               <a href="/register" style={{marginLeft: '5%'}}>go to register</a>
             </span>
             </div>
