8850b480251dcb7299d994e56f96cf043cff9346
rikosellic <514677096@qq.com>
2020-06-11 23:03:48 +0800

    导航栏功能更新

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -15,11 +15,11 @@ const NavBar = (props) => {
     <header>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/main" style={{marginLeft:"-15%"}}><Image src={logo} className="navlogo1" fluid/></Navbar.Brand>
+          <Navbar.Brand href={"/main/"+props.myname} style={{marginLeft:"-15%"}}><Image src={logo} className="navlogo1" fluid/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/personal">Personal Information</Nav.Link>
+              <Nav.Link href="/personal">Hello, {props.myname}</Nav.Link>
               <Nav.Link href="/personal">Sign Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
