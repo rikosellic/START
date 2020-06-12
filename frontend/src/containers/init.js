88a92fa2ab36c35b64a706356f74395c530bf7b9
lwl000 <1078607150@qq.com>
2020-06-12 21:34:38 +0800

    no message

--- frontend/src/containers/init.js
+++ frontend/src/containers/init.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import NavBar from "../components/Nav";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+} from 'react-bootstrap';
+import "./reviewSummary.css";
+import "./href.css";
+import Logo from "../components/Logo";
+import NavBar2 from "../components/Nav2";
+import Footer from "../components/Footer";
+class studySummary extends React.Component {
+	login()
+	{
 
-class Init extends React.Component {
+        const url = " http://localhost:8000/api/login";
+           fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-type":"application/json;charset=utf-8",
+                },
+                body: JSON.stringify(),
+           }).then(res=>{
+			   this.props.history.push('/login');
+           })
+    }
+	register()
+	{
+        const url = " http://localhost:8000/api/userinfomanagement";
+           fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-type":"application/json;charset=utf-8",
+                },
+                body: JSON.stringify(),
+           }).then(res=>{
+			   this.props.history.push('/register');
+           })
+    }
   render() {
     return (
+	<html>
       <div>
-        <h1>This is Init!</h1>
+              <div className='login-logo' ><Logo/></div>
+                <Row>
+                  <Col>
+                  </Col>
+                  <div class="main-button">
+                    <Button variant="primary" size="lg" class="main-button" onClick={this.login.bind(this)}>登录</Button>
+                  </div>
+                  <div class="main-button">
+                    <Button variant="primary" size="lg" class="main-button" onClick={this.register.bind(this)}>注册</Button>
+                  </div>
+                  <Col>
+                  </Col>
+                </Row>
       </div>
+	</html>
     );
   }
 }
 
-export default Init;
+export default studySummary;
